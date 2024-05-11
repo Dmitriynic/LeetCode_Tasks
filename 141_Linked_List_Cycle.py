31f2fc0a12541726b734f86fa638c564654d005c
Dmitriynic <silverbackstrela2@gmail.com>
2024-05-11 08:39:29 -0400

    Task №141

---
+++ 141_Linked_List_Cycle.py
@@ -0,0 +1,19 @@
+#141 Linked List Cycle
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        while fast and fast.next:
+            head = head.next
+            fast = fast.next.next
+            if head == fast:
+                return True
+        return False
+
+#47ms faster than 46.31%, memory 19.1MB less than 73.59%
