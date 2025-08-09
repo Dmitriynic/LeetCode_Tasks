19e30323f27e2940846e74cd50071839a380d7ed
Dmitriynic <silverbackstrela2@gmail.com>
2025-08-09 19:45:08 +0300

    Implement_Trie

---
+++ 208_Implement_Trie.py
@@ -0,0 +1,47 @@
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.isWord = True
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.isWord
+        
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
+
+# faster than 37.28%, memory less than 72.51%
